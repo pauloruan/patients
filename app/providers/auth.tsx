cbc957c04d8a10cab628e82d1aac26cae6f76348
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-02 00:12:08 -0300

    chore: Remove auth provider

--- app/providers/auth.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { SessionProvider, SessionProviderProps } from "next-auth/react"
-
-function AuthProvider({ children }: SessionProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>
-}
-
-export default AuthProvider
--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -1,12 +1,9 @@
-import AuthProvider from "./auth"
 import ThemeProvider from "./theme"
 
 export function Providers({ children }: IProvidersProps): JSX.Element {
   return (
     <ThemeProvider>
-      <AuthProvider>
       {children}
-      </AuthProvider>
     </ThemeProvider>
   )
 }
