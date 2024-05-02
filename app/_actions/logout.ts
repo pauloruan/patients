02b24cc407ab16ff535f3deb666c822c4e94ccdd
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-02 00:14:48 -0300

    fix: Fix logout to delete user cookies

--- app/_actions/logout.ts
+++ app/_actions/logout.ts
@@ -1,10 +1,18 @@
 "use server"
 
 import { signOut } from "@/auth"
+import { cookies } from "next/headers"
 
 export default async function logout() {
+  const cookieStore = cookies()
+
   try {
     await signOut()
+    const verifyUserIdCookie = cookieStore.has("userId")
+    if (verifyUserIdCookie) {
+      cookieStore.delete("userId")
+      cookieStore.delete("email")
+    }
   } catch (e) {
     throw e
   }
