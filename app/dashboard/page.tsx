407893afc68fb57c7aa74acccb1dfacc12dce0fe
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-14 13:29:16 -0300

    chore: Update DashboardPage to use DashboardContent component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,18 @@
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
 import { Content } from "../_components/content"
-import { Info } from "./_components/info"
+import { DashboardContent } from "./_components/DashboardContent"
 
 export default async function DashboardPage() {
-  let user = undefined
   const session = await auth()
 
-  if (session) {
-    user = session.user
-  } else {
+  if (!session) {
     redirect("/")
   }
 
   return (
     <Content>
-      <Info user={user} />
+      <DashboardContent />
     </Content>
   )
 }
