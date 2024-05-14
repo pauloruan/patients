500cb75ce0603d3f16f12912544a71cd583545ea
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-13 21:19:23 -0300

    chore: Refactor logout to remove unused code and improve cookie handling

--- app/_actions/logout.ts
+++ app/_actions/logout.ts
@@ -1,18 +1,10 @@
 "use server"
 
 import { signOut } from "@/auth"
-import { cookies } from "next/headers"
 
 export default async function logout() {
-  const cookieStore = cookies()
-
   try {
     await signOut()
-    const verifyUserIdCookie = cookieStore.has("userId")
-    if (verifyUserIdCookie) {
-      cookieStore.delete("userId")
-      cookieStore.delete("email")
-    }
   } catch (e) {
     throw e
   }
