dc6a17bc41c64f2cf4a59ebfdb7c493d7c90f133
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-13 21:17:08 -0300

    chore: Update PrismaAdapter to use the new prisma import

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth, { NextAuthConfig } from "next-auth"
 import Google from "next-auth/providers/google"
-import { db } from "./app/lib/prisma"
+import { prisma } from "@/app/lib/prisma"
 
 const config: NextAuthConfig = {
-  adapter: PrismaAdapter(db),
+  adapter: PrismaAdapter(prisma),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
