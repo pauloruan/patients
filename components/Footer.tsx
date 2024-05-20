b96b2233f73a1b9524c8dac6d7d478043a3405e7
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-05-20 14:04:19 -0300

    chore: Update footer to include copyright information

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,17 +5,14 @@ export function Footer() {
   return (
     <footer className="flex h-16 w-full items-center justify-center bg-background dark:bg-background">
       <Typography.P className="text-xs">
-        Feito com&nbsp;
-        <span role="img" aria-label="love">
-          ❤️
-        </span>
-        &nbsp;por&nbsp;
+        Made by&nbsp;
         <Link
           href="https://pauloruan.dev"
           className="font-bold hover:underline"
         >
           Paulo Ruan
         </Link>
+        . All rights reserved.
       </Typography.P>
     </footer>
   )
